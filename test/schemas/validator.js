aa7d1e361a19373db6462f2841f258815f05b47d
Matt Dawson <mattldawson@gmail.com>
2020-03-12 16:31:31 -0700

    add some properties to chemical species schema

--- test/schemas/validator.js
+++ test/schemas/validator.js
@@ -1,21 +1,34 @@
 'use strict';
 
-if( process.argv.length != 4 ) {
-  console.log('Useage: node validator.js my_schema.json my_data.json');
+if( process.argv.length != 3 ) {
+  console.log('Useage: node validator.js my_data.json');
   exit;
 }
 
 const Ajv = require('ajv');
 const fs = require('fs');
 
-let raw_json = fs.readFileSync( process.argv[2] );
-let schema = JSON.parse( raw_json );
+let raw_json = fs.readFileSync( '../../schemas/chemical_species.json' );
+let chemical_species_schema = JSON.parse( raw_json );
 
-raw_json = fs.readFileSync( process.argv[3] );
+raw_json = fs.readFileSync( '../../schemas/chemical_species_standard_properties/molecular_weight.json' );
+let molecular_weight_schema = JSON.parse( raw_json );
+
+raw_json = fs.readFileSync( '../../schemas/chemical_species_standard_properties/henrys_law_constant.json' );
+let henrys_law_constant_schema = JSON.parse( raw_json );
+
+raw_json = fs.readFileSync( '../../schemas/chemical_species_standard_properties/diffusion_coefficient.json' );
+let diffusion_coefficient_schema = JSON.parse( raw_json );
+
+raw_json = fs.readFileSync( process.argv[2] );
 let test_data = JSON.parse( raw_json );
 
-let ajv = new Ajv( );
-let valid = ajv.validate( schema, test_data );
+let ajv = new Ajv({ verbose : true });
+let validate = ajv.addSchema( molecular_weight_schema      )
+                  .addSchema( henrys_law_constant_schema   )
+                  .addSchema( diffusion_coefficient_schema )
+                  .compile(   chemical_species_schema      );
+let valid = validate( test_data );
 if( !valid ) {
   console.log( ajv.errorsText( ) );
   console.log( 'FAIL' );
